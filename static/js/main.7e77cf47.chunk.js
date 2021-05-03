0741b2c46bf0eccaa38b4035cc503161369cc240
Amnaaftab123 <sp19-rcs-062@student.comsats.edu.pk>
2021-05-04 04:29:04 +0500

    Updates

---
+++ static/js/main.7e77cf47.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-practice"]=this["webpackJsonpreact-practice"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var r=c(1),n=c.n(r),i=c(3),a=c.n(i),s=c(0);var o=function(){return Object(s.jsx)("div",{children:"Henllo"})};a.a.render(Object(s.jsx)(n.a.StrictMode,{children:Object(s.jsx)(o,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.7e77cf47.chunk.js.map
